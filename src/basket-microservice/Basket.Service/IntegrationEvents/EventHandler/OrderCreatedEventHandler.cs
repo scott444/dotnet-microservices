38792c0768a001afdd276bc156da4167a57a7494
Scott Howell <me@email.com>
2024-10-18 14:12:01 -0600

    Add event and reference shared lib

---
+++ src/basket-microservice/Basket.Service/IntegrationEvents/EventHandler/OrderCreatedEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using Basket.Service.Infrastructure.Data;
+using ECommerce.Shared.Infrastructure.EventBus.Abstractions;
+
+namespace Basket.Service.IntegrationEvents.EventHandlers;
+
+internal class OrderCreatedEventHandler(IBasketStore basketStore) : IEventHandler<OrderCreatedEvent>
+{
+    private readonly IBasketStore _basketStore = basketStore;
+
+    public Task Handle(OrderCreatedEvent @event)
+    {
+        _basketStore.DeleteCustomerBasket(@event.CustomerId);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ src/basket-microservice/Basket.Service/IntegrationEvents/OrderCreatedEvent.cs
@@ -0,0 +1,5 @@
+﻿using ECommerce.Shared.Infrastructure.EventBus;
+
+namespace Basket.Service.IntegrationEvents;
+
+public record OrderCreatedEvent(string CustomerId) : Event;
