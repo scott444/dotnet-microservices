5048208e8e0471ce4ffe797729f0a8ed287449fe
Scott Howell <me@email.com>
2024-10-18 15:19:19 -0600

    update eventbus

--- src/basket-microservice/Basket.Service/Program.cs
+++ src/basket-microservice/Basket.Service/Program.cs
@@ -1,11 +1,19 @@
 using Basket.Service.Endpoints;
 using Basket.Service.Infrastructure.Data;
+using Basket.Service.IntegrationEvents;
+using Basket.Service.IntegrationEvents.EventHandlers;
+using ECommerce.Shared.Infrastructure.EventBus;
+using ECommerce.Shared.Infrastructure.RabbitMq;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IBasketStore, InMemoryBasketStore>();
+
+builder.Services.AddRabbitMqEventBus(builder.Configuration)
+    .AddRabbitMqSubscriberService(builder.Configuration)
+    .AddEventHandler<OrderCreatedEvent, OrderCreatedEventHandler>();
 
 var app = builder.Build();
 
 app.RegisterEndpoints();
 
-app.Run();
+await app.RunAsync();
